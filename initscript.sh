48d65554fa70f31c7b5a7b1cf6b16a3a3d688613
Oli Zimpasser <oglimmer@gmail.com>
2023-02-03 22:13:03 +0100

    Update initscript.sh

--- initscript.sh
+++ initscript.sh
@@ -5,7 +5,7 @@ set -eu
 if [ -z "${1:-}" ]; then
 
 	while true; do
-		/usr/sbin/logrotate -v -f --state /tmp/logrotate.status /etc/logrotate.conf
+		/usr/sbin/logrotate -v -f --state /tmp/logrotate.status /etc/logrotate/logrotate.conf
 		sleep 86400
 	done
 
